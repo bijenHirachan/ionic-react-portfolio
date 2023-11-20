ca53300e31e636dfd734fb2ff5c53dc0c565b310
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-11-20 01:37:17 +0100

    Js words commented

--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -76,24 +76,26 @@ const Home = () => {
                 fontFamily: "Agbalumo",
             }
         );
-        gsap.fromTo(
-            jsRef.current,
-            {
-                x: -400,
-            },
-            {
-                x: 400,
-                duration: 9,
-                repeat: -1,
-                yoyo: true,
-            }
-        );
+
+        // var tl2 = gsap.timeline();
+        // tl2.fromTo(
+        //     jsRef.current,
+        //     {
+        //         x: -100,
+        //     },
+        //     {
+        //         x: 100,
+        //         duration: 9,
+        //         repeat: -1,
+        //         yoyo: true,
+        //     }
+        // );
     }, []);
 
     return (
         <Welcome>
             <div className="h-full flex flex-col gap-6 justify-between items-center z-10">
-                <div className="absolute bottom-[0svh] w-[200svh] opacity-20 font-abeeze">
+                {/* <div className="absolute bottom-[0svh] w-[200svh] opacity-20 font-abeeze">
                     <div
                         ref={jsRef}
                         className="flex font-semibold h-full w-full flex-wrap items-center justify-around"
@@ -109,7 +111,7 @@ const Home = () => {
                         <div className="text-mylight">MongoDB</div>
                         <div className="text-mylight">HTML5</div>
                     </div>
-                </div>
+                </div> */}
 
                 <div className="h-full w-full flex flex-col justify-center items-center relative z-10">
                     <Canvas>
@@ -141,7 +143,7 @@ const Home = () => {
                         </p>
                     </div>
 
-                    <div className="absolute bottom-8 right-4 flex flex-col items-end">
+                    <div className="absolute bottom-2 right-2 flex flex-col items-end pl-1">
                         <div
                             ref={quoteRef}
                             className="shadow-md text-sm opacity-0 bg-mygreen rounded-md mb-4 text-mylight grid grid-cols-12"
@@ -178,73 +180,6 @@ const Home = () => {
             </div>
         </Welcome>
     );
-
-    // return (
-    //     <Welcome>
-    //         <div className="h-full flex flex-col gap-6 justify-center items-start relative">
-    //             <div className="flex flex-col gap-4 items-start">
-    //                 <h1
-    //                     ref={hiRef}
-    //                     className=" text-indigo-50 mb-8 text-[64px] font-[900] drop-shadow-xl"
-    //                 >
-    //                     Hi!
-    //                 </h1>
-    //                 <h1
-    //                     ref={nameRef}
-    //                     className="text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-red-500 text-[32px] sm:text-[64px] font-[900] drop-shadow-xl"
-    //                 >
-    //                     I'm Bijen Hirachan
-    //                 </h1>
-
-    //                 <p
-    //                     ref={jobRef}
-    //                     className="text-neutral-700 text-lg md:text-[40px] md:mt-8 font-semibold"
-    //                 >
-    //                     A Fullstack Developer
-    //                 </p>
-    //             </div>
-
-    //             <div className="absolute bottom-0 right-0 flex flex-col items-end">
-    //                 {/* {quote?.data && (
-
-    //                 )} */}
-    //                 <div
-    //                     ref={quoteRef}
-    //                     className="shadow-md text-sm opacity-0 bg-indigo-200 rounded-md mb-4 text-neutral-800 grid grid-cols-12"
-    //                 >
-    //                     <div className="col-span-11 px-2 py-1 flex flex-col items-end">
-    //                         <p>{quote?.data?.content}</p>
-
-    //                         <p>
-    //                             <a
-    //                                 target="_blank"
-    //                                 href={quote?.data?.originator?.url}
-    //                             >
-    //                                 -{quote?.data?.originator?.name}
-    //                             </a>
-    //                         </p>
-    //                     </div>
-    //                     <div className="border-indigo-600/50  border-l-2 col-span-1 flex justify-center items-center">
-    //                         <AiOutlineClose
-    //                             onClick={closeQuote}
-    //                             className="cursor-pointer"
-    //                         />
-    //                     </div>
-    //                 </div>
-
-    //                 <p className="font-semibold text-sm mb-2">
-    //                     Want to read a quote?
-    //                 </p>
-    //                 <button
-    //                     onClick={fetchQuote}
-    //                     className="text-xs uppercase text-white font-semibold px-2 py-1 bg-gradient-to-r from-indigo-500 to-red-400 hover:-translate-x-2 transition-all delay-75"
-    //                 >
-    //                     Click Me!
-    //                 </button>
-    //             </div>
-    //         </div>
-    //     </Welcome>
-    // );
 };
 
 export default Home;
